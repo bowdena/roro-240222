e2ae99e05c6e560b73fba5742466d0a23ee0bab1
Andrew Bowden <andrew.bowden@minigeek.org>
2024-02-20 20:24:36 +1100

    Setup the initial stimulus controller

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 rails _7.1.3_ new stimulus_20240219 --css tailwind --javascript=esbuild --asset-pipeline=propshaft
 rails g controller StaticPages roro
 root 'static_pages#roro'
+
+### Create the visibility stimulus controller
+rails g stimulus Visibility
+https://stimulus.hotwired.dev/reference/lifecycle-callbacks
---
+++ app/javascript/controllers/visibility_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="visibility"
+export default class extends Controller {
+
+  static targets = ["infodiv"]
+
+  connect() {
+    console.log("Connecting to the visibility controller")
+  }
+
+  toggle(event){
+    event.preventDefault();
+    console.log("Toggling visibility")
+    console.log(this.infodivTarget)
+    this.infodivTarget.classList.toggle("hidden")
+  }
+}
